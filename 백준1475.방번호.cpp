188bf6fa35504f31dae7e27ca48f0cd5507bdf95
gaeun6883 <93725108+gaeun6883@users.noreply.github.com>
2021-11-29 17:15:47 +0900

    Create 백준1475.방번호.cpp

---
+++ 백준1475.방번호.cpp
@@ -0,0 +1,34 @@
+//mentoring 3. 2021.11.1
+//배열과 벡터
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string s;
+	cin >> s;
+	int setList[10];//vector<int> setList;
+	for (int i = 0; i < 10; i++)
+		setList[i] = 0;//setList.push_back(0);
+
+	for (int x = 0; x < s.length(); x++) {
+		int number = s[x]-'0';
+		++setList[number];
+	}
+
+	setList[6] = (setList[6] + setList[9]) / 2 + (setList[6] + setList[9]) % 2;
+	setList[9] = 0;
+
+	int max = 0;
+	for (int y = 0; y < 9; y++) {
+		if (max < setList[y])
+			max = setList[y];
+	}
+	printf("%d", max);
+	return 0;
+}
