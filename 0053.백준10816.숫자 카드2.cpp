8a457c27ce84f0a6feddafd5c6cec4aba50b7e9b
gaeun6883 <93725108+gaeun6883@users.noreply.github.com>
2022-01-07 21:12:52 +0900

    Create 0053.백준10816.숫자 카드2.cpp

---
+++ 0053.백준10816.숫자 카드2.cpp
@@ -0,0 +1,54 @@
+//mentoring 53.2022.1.7
+//이진탐색 응용
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> list;
+
+int minIdx(int target) {
+    int left = 0, right = list.size() - 1;
+    while (left <= right) {
+        int middle = (left + right) / 2;
+        if (list[middle] >= target) {//list[middle]�� target���� ũ�ų� �����Ƿ� right�� ���̱�
+            right = middle - 1;
+        }
+        else left = middle + 1;//list[middle]�� ������ target���� �����Ƿ� left�� �ø���
+    }
+    return left;
+}
+
+int maxIdx(int target) {
+    int left = 0, right = list.size() - 1;
+    while (left <= right) {
+        int middle = (left + right) / 2;
+        if (list[middle] <= target) {
+            left = middle + 1;
+        }
+        else right = middle - 1;
+    }
+    return right;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, M;
+    cin >> N;
+    int n;
+    for (int i = 0; i < N; i++) {
+        cin >> n;
+        list.push_back(n);
+    }
+    sort(list.begin(), list.end());
+    cin >> M;
+    for (int i = 0; i < M; i++) {
+        cin >> n;
+        int ans = maxIdx(n) - minIdx(n) + 1;
+        cout << ans << " ";
+    }
+    return 0;
+}
